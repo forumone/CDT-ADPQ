fdc3cf1ce97b29620ac5dbe4d0065133756882d8
William Hurley <whurley@forumone.com>
2017-03-01 10:46:18 -0500

    Fixing issue with invalid providers

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,18 @@
-var app = angular.module('f1CdtAdpq', ['ngRoute', 'ui.router', 'config', 'ngMessages']);
-app.config(function($locationProvider, $mdThemingProvider, $qProvider) {
+var app = angular.module('f1CdtAdpq', ['ngRoute', 'ui.router']);
+app.config(function($locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
 })
-.run(function(parseApplicationId, parseJavascriptKey, serverUrl, $state, $location) {
- Parse.initialize(parseApplicationId, parseJavascriptKey);
-
- Parse.serverURL = serverUrl;
-  
-  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
-    var redirect = (angular.isDefined(toParams.redirect) && toParams.redirect) ? toParams.redirect : encodeURIComponent($location.url());
-    
-    $state.go('userLogin', {redirect: redirect});
-    // this is required if you want to prevent the $UrlRouter
-    // reverting the URL to the previous valid location
-    event.preventDefault();
-  });
+.run(function($state, $location) {
+// Parse.initialize(parseApplicationId, parseJavascriptKey);
+//
+// Parse.serverURL = serverUrl;
+//  
+//  $rootScope.$on('$stateChangeError', function(event, toState, toParams, fromState, fromParams, error) {
+//    var redirect = (angular.isDefined(toParams.redirect) && toParams.redirect) ? toParams.redirect : encodeURIComponent($location.url());
+//    
+//    $state.go('userLogin', {redirect: redirect});
+//    // this is required if you want to prevent the $UrlRouter
+//    // reverting the URL to the previous valid location
+//    event.preventDefault();
+//  });
 });
