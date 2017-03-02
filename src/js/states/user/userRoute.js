fbebb71a6313074d09885825fc998638d6b0d1d6
William Hurley <whurley@forumone.com>
2017-03-02 14:39:50 -0500

    [ADQP-26] Wiring up user login/logout/registration to Parse

--- src/js/states/user/userRoute.js
+++ src/js/states/user/userRoute.js
@@ -1,13 +1,18 @@
-angular.module('f1CdtAdpq').config(function($urlRouterProvider, $stateProvider) {
+angular.module('f1CdtAdpq').config(function($stateProvider) {
   $stateProvider
   .state('user', {
     url: '/user',
     templateUrl: 'states/user/user.html',
-    controller: 'UserController',
+    controller: 'UserController'
   })
   .state('userRegister', {
     url: '/user/register',
     templateUrl: 'states/user/states/register/register.html',
-    controller: 'UserRegisterController',
+    controller: 'UserRegisterController'
+  })
+  .state('userLogout', {
+    url: '/user/logout',
+    templateUrl: 'states/user/states/logout/logout.html',
+    controller: 'UserLogoutController',
   });
 });
